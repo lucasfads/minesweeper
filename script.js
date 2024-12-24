f22b5bae811487df17cff1d48ded75dd253c7712
Lucas Fernandes <lucasfads@gmail.com>
2024-12-24 13:15:06 -0300

    Add wrong indicator for non-bomb cells on game over and update footer link target

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ class Minesweeper {
 	static FLAG_MASK = 0b1000000;		// 01000000 = 64
 	static BOMB_SVG = `<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-bomb"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14.499 3.996a2.2 2.2 0 0 1 1.556 .645l3.302 3.301a2.2 2.2 0 0 1 0 3.113l-.567 .567l.043 .192a8.5 8.5 0 0 1 -3.732 8.83l-.23 .144a8.5 8.5 0 1 1 -2.687 -15.623l.192 .042l.567 -.566a2.2 2.2 0 0 1 1.362 -.636zm-4.499 5.004a4 4 0 0 0 -4 4a1 1 0 0 0 2 0a2 2 0 0 1 2 -2a1 1 0 0 0 0 -2z" /><path d="M21 2a1 1 0 0 1 .117 1.993l-.117 .007h-1c0 .83 -.302 1.629 -.846 2.25l-.154 .163l-1.293 1.293a1 1 0 0 1 -1.497 -1.32l.083 -.094l1.293 -1.292c.232 -.232 .375 -.537 .407 -.86l.007 -.14a2 2 0 0 1 1.85 -1.995l.15 -.005h1z" /></svg>`;
 	static FLAG_SVG = `<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-flag"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 5a1 1 0 0 1 .3 -.714a6 6 0 0 1 8.213 -.176l.351 .328a4 4 0 0 0 5.272 0l.249 -.227c.61 -.483 1.527 -.097 1.61 .676l.005 .113v9a1 1 0 0 1 -.3 .714a6 6 0 0 1 -8.213 .176l-.351 -.328a4 4 0 0 0 -5.136 -.114v6.552a1 1 0 0 1 -1.993 .117l-.007 -.117v-16z" /></svg>`;
+	static WRONG_SVG = `<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>`;
 
 	constructor (rows, cols, mines) {
 		this.rows = rows;
@@ -63,7 +64,7 @@ class Minesweeper {
 	}
 
 	isBomb (x, y) {
-		if (this.board[y][x] == Minesweeper.BOMB_MASK)
+		if ((this.board[y][x] & Minesweeper.BOMB_MASK) == Minesweeper.BOMB_MASK)
 			return (true);
 		return (false);
 	}
@@ -138,6 +139,8 @@ class Minesweeper {
 				{
 					cellElement.classList.add("flag");
 					cellElement.innerHTML = Minesweeper.FLAG_SVG;
+					if (this.gameOver && !this.isBomb(x, y))
+						cellElement.innerHTML = Minesweeper.WRONG_SVG;
 				}	
 				if (this.isCellRevealed(x, y))
 				{
